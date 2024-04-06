4652594a885106d6f96d939acc61d64c3aec3755
shreyasrox2326 <110967852+shreyasrox2326@users.noreply.github.com>
2024-04-06 19:35:28 +0530

    Add files via upload

--- sierp-anim.py
+++ sierp-anim.py
@@ -3,6 +3,15 @@ import numpy as np
 import random
 import matplotlib.animation as animation
 
+
+a=[[',',(1000,)], ['.',(1,)]]
+choice=int(input("Enter choice 1 or 2\n1 gives a large number of points but each point is tiny\n2 gives a lesser number of points but each point is bigger\n"))
+ 
+#choice can be 1 or 2,
+#1 gives a large number of points but each point is tiny
+#2 gives a lesser number of points but each point is bigger
+
+
 xx=[1, 11, 6]
 yy=[1, 1, 1+5*3**0.5]
 
@@ -10,7 +19,7 @@ p=[[1,1],[11,1],[6,1+5*3**0.5]]
 z=[2,2]
 
 fig, ax = plt.subplots()
-line, = ax.plot(xx, yy, ',')
+line, = ax.plot(xx, yy, a[choice-1][0])
 
 xx_anim = []
 yy_anim = []
@@ -34,6 +43,9 @@ def update(frame, num_points):
     ax.set_xlim([min(xx_anim), max(xx_anim)])
     ax.set_ylim([min(yy_anim), max(yy_anim)])
 
-ani = animation.FuncAnimation(fig, update, frames=500, interval=30, fargs=(1000,))
+
+
+ani = animation.FuncAnimation(fig, update, frames=200, interval=0, fargs=a[choice-1][1])
+
 
 plt.show()
