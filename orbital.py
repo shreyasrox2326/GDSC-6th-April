f9d5c24507fe2b07e1e5f026c40d06097c59d6e7
shreyasrox2326 <110967852+shreyasrox2326@users.noreply.github.com>
2024-04-06 19:10:17 +0530

    Add files via upload

--- orbital.py
+++ orbital.py
@@ -1,5 +1,5 @@
-#3p orbital plot
-# 3p because it has radial and angular nodes and looks cool
+#3px orbital plot
+# 3px because it has radial and angular nodes and looks cool
 
 
 import math
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 point=','
 x=0
-res=16
+res=4 #resolution variable, keep 4 for fast plot, 8 or 16 for higher resolution (16 takes 2 minutes to plot not recommended)
 y=10**-7/(res*100)
 z=10**-7/(res*100)
-
+#colourss variable contains list of colours for getting a gradient according to probability density
 colourss=['#9E0142' ,'#D53E4F', '#F46D43', '#FDAE61' ,'#FEE08B' ,'#FFFFBF', '#E6F598', '#ABDDA4', '#66C2A5', '#3288BD' ,'#5E4FA2','#2D004B']
 
+#to fill in the colours for each pixel according to the probablity density function
 for q in range(12):
     ypoints=[]
     zpoints=[]
@@ -23,19 +24,21 @@ for q in range(12):
             phi=0
             a0=52.9*10**-10
             e=math.e
-            r=(yy*yy+zz*zz)**0.5
+            r=(yy*yy+zz*zz)**0.5 #converting x and y to r and theta
             theta=math.asin(zz/r)
+            #psi**2 is the probabilty density function
             psi=1/(81*(6*math.pi*a0**5)**0.5)*r/a0*(6-r/a0)*e**(-r/(3*a0))*math.sin(theta)
             if 10**(35+q*0.25-0.25)<psi**2<10**(35+q*0.25+0.25-0.25):
                 ypoints.append(yy)
                 zpoints.append(zz)
-
+    #plot colour according to value of psi**2
     yp = np.array(ypoints)
     zp = np.array(zpoints)
     plt.plot(zp, yp, point,color=colourss[q])
 
 ypoints=[]
 zpoints=[]
+# to colour the remaining area black
 for i in range(-124*res+1,124*res,2):
     for j in range(-150*res+1,150*res,2):
         yy=i*y
@@ -52,7 +55,7 @@ for i in range(-124*res+1,124*res,2):
 
 yp = np.array(ypoints)
 zp = np.array(zpoints)
-
+#colour the remaining area black
 plt.plot(zp, yp, point,color='#000000')
 
 plt.show()
